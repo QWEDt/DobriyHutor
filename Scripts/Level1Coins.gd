6f0c18585d66580feb2084db74c99c7ef7b18176
QWEDt <64944732+QWEDt@users.noreply.github.com>
2024-04-27 21:57:56 +0500

    level 1 and 2 improvements

--- Scripts/Level1Coins.gd
+++ Scripts/Level1Coins.gd
@@ -6,8 +6,16 @@ func _process(delta):
 	if coins_count > 0:
 		return
 	
-	TextLabel.show_text("LEVEL COMPLITED")
-	coins_count = 999999
+	TextLabel.show_text("LEVEL COMPLITED", 45)
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(5)
+	await timer.timeout
+	change_to_level_2()
+
+func change_to_level_2():
+	get_tree().change_scene_to_file("res://Scenes/levels/level_2.tscn")
+	
 
 func _on_child_exiting_tree(node):
 	coins_count -= 1
--- Scripts/LevelText.gd
+++ Scripts/LevelText.gd
@@ -2,11 +2,14 @@ extends CanvasLayer
 
 @onready var label = $Label
 
+signal text_showed
+
 func _ready():
 	label.visible = false
 
-func show_text(text: String):
+func show_text(text: String, font_size: int = 140):
 	var timer = Timer.new()
+	label.label_settings.font_size = font_size
 	add_child(timer)
 	timer.start(1.5)
 	await timer.timeout
@@ -16,3 +19,4 @@ func show_text(text: String):
 	await timer.timeout
 	timer.queue_free()
 	label.visible = false
+	text_showed.emit()
