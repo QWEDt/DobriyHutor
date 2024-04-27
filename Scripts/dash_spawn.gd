3612456b4efb4e90169f576ee594b2d0f6835c17
QWEDt <64944732+QWEDt@users.noreply.github.com>
2024-04-28 01:36:33 +0500

    level 3

---
+++ Scripts/dash_spawn.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var player: CharacterBody2D
+@onready var gpu_particles_2d = $GPUParticles2D
+
+signal on_dash_get
+
+func _ready():
+	if !player:
+		return
+	
+	go_particles()
+	on_dash_get.connect(player.geting_dash)
+
+func go_particles():
+	if !player:
+		return
+	gpu_particles_2d.emitting = true
+	var length = (global_position - player.global_position).length()
+	gpu_particles_2d.amount = \
+ 	30000 / (length * length)
+	gpu_particles_2d.one_shot = true
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(6)
+	await timer.timeout
+	go_particles()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		on_dash_get.emit()
+		player = null
